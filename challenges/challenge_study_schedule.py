d9cda0ebdb8fa3e92e0c1c32f57515114b713f63
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-02 16:47:39 -0300

    feat: implementa função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_online = 0
+    if not target_time:
+        return None
+    for start_time, end_time in permanence_period:
+        if type(start_time) != int:
+            return None
+        if type(end_time) != int:
+            return None
+        if start_time <= target_time <= end_time:
+            students_online += 1
+    return students_online
