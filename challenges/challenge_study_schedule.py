3d6a88859cc2b076e717d970ced3c011c9d744fa
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-02 16:51:48 -0300

    fix: reduz complexidade
    Valida start_time e end_time em um único if.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,9 +3,7 @@ def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
     for start_time, end_time in permanence_period:
-        if type(start_time) != int:
-            return None
-        if type(end_time) != int:
+        if type(start_time) != int or type(end_time) != int:
             return None
         if start_time <= target_time <= end_time:
             students_online += 1
